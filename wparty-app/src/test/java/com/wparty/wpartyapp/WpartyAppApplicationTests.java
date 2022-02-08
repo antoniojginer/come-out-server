c50baab8fbe32d2a05a525218cf4f8551f73ff80
Antonio Giner <aginer@okode.com>
2022-02-08 20:06:52 +0100

    Project estructure created

---
+++ wparty-app/src/test/java/com/wparty/wpartyapp/WpartyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wparty.wpartyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpartyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
